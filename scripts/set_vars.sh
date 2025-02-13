93b6bf8013abb1b35389b89715ec219ebcd70ab8
SnailZ1 <104450968+SnailZ1@users.noreply.github.com>
2025-02-13 17:34:51 +0300

    fix versions

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -19,7 +19,15 @@ export SC_MACHINE_REPO="${SC_MACHINE_REPO:-https://github.com/ostis-ai/sc-machin
 export SC_MACHINE_BRANCH="${SC_MACHINE_BRANCH:-main}"
 export SC_MACHINE_COMMIT="${SC_MACHINE_COMMIT:-"d9b4ef25e39ece566d1b5f0931b8d2f3354542ad"}"
 
+export SCP_MACHINE_REPO="${SCP_MACHINE_REPO:-https://github.com/ostis-ai/scp-machine.git}"
+export SCP_MACHINE_BRANCH="${SCP_MACHINE_BRANCH:-main}"
+export SCP_MACHINE_COMMIT="${SCP_MACHINE_COMMIT:-"ee177932acc70680e04858c232378bd52b3b5ecf"}"
+
+export SC_COMPONENT_MANAGER_REPO="${SC_COMPONENT_MANAGER_REPO:-https://github.com/ostis-ai/sc-component-manager.git}"
+export SC_COMPONENT_MANAGER_BRANCH="${SC_COMPONENT_MANAGER_BRANCH:-main}"
+export SC_COMPONENT_MANAGER_COMMIT="${SC_COMPONENT_MANAGER_COMMIT:-"b82f62f59dbce6cc9b611714a4b4cdc3c6e33a43"}"
+
 if [[ -d "${PLATFORM_PATH}" ]];
 then
-  source "${PLATFORM_PATH}/scripts/set_vars.sh"
+  source "${PLATFORM_PATH}/scripts/set_vars.sh"     
 fi
