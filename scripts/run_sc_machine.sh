3eb1d60f5b168cf704c773205fb6d6095c8c9410
Natashka Malinovskaya <63506873+Malina09@users.noreply.github.com>
2025-05-05 14:01:37 +0300

    remove comments from run_sc_machine.sh

--- scripts/run_sc_machine.sh
+++ scripts/run_sc_machine.sh
@@ -1,17 +1,6 @@
-# #!/usr/bin/env bash
-# set -eo pipefail
-
-# if [ -z "${PLATFORM_PATH}" ];
-# then
-#   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/set_vars.sh
-# fi
-
-# "${PLATFORM_PATH}/scripts/run_sc_machine.sh" "$@"
-
+#!/usr/bin/env bash
 set -eo pipefail
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 
 "${SC_MACHINE_PATH}/build/Release/bin/sc-machine" -c "${CONFIG_PATH}" \
   -e "${SC_MACHINE_PATH}/build/Release/lib/extensions;${SCP_MACHINE_PATH}/build/Debug/lib/extensions"
-
-#run_cxx_ps.sh
